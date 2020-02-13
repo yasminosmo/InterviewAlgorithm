8eb28f5deda17ad8bd0051d818dcb1eb86c5bf0a
yasmin <yasmin@clouperp.io>
2020-02-14 00:39:16 +0200

    Add project files.

---
+++ InterviewAlgorithm/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InterviewAlgorithm.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ InterviewAlgorithm/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InterviewAlgorithm.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
